13b34c915ea541dec96b3ec6f98fc3b1c700d760
ag-jod00 <abhinandan02102010@gmail.com>
2025-02-21 23:51:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,93 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const moment = require('moment');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = socketIo(server);
 
-app.use(express.static(__dirname)); // Serve static files
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+    session({
+        secret: "secret_key",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    })
+);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+// MongoDB Connection
+mongoose.connect("mongodb://127.0.0.1:27017/messengerDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-let users = {};
+// User Schema
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+});
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+const User = mongoose.model("User", userSchema);
 
-    socket.on('join', (username) => {
-        users[socket.id] = username;
-        io.emit('updateUsers', Object.values(users));
-        console.log(`${username} joined the chat`);
+// Message Schema
+const messageSchema = new mongoose.Schema({
+    sender: String,
+    content: String,
+    timestamp: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+// Serve Static Files
+app.use(express.static(__dirname));
+
+// User Authentication
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        req.session.user = user.name;
+        res.cookie("username", user.name, { maxAge: 86400000 }); // Save name in cookie
+        res.json({ success: true, name: user.name });
+    } else {
+        res.json({ success: false, message: "Invalid credentials" });
+    }
+});
+
+// Register Default User (Only for First Time)
+async function createDefaultUser() {
+    const existingUser = await User.findOne({ username: "chat01" });
+    if (!existingUser) {
+        const hashedPassword = await bcrypt.hash("0000", 10);
+        await User.create({ username: "chat01", password: hashedPassword, name: "Default User" });
+        console.log("Default user created.");
+    }
+}
+createDefaultUser();
+
+// Socket.io for Real-time Messaging
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on("sendMessage", async (data) => {
+        const newMessage = new Message({ sender: data.sender, content: data.message });
+        await newMessage.save();
+        io.emit("newMessage", { sender: data.sender, message: data.message });
     });
 
-    socket.on('chatMessage', (data) => {
-        io.emit('chatMessage', { username: data.username, message: data.message, time: moment().format('h:mm A') });
-    });
-
-    socket.on('typing', (username) => {
-        socket.broadcast.emit('typing', username);
-    });
-
-    socket.on('disconnect', () => {
-        console.log(`${users[socket.id]} left the chat`);
-        delete users[socket.id];
-        io.emit('updateUsers', Object.values(users));
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-});
+server.listen(3000, () => console.log("Server running on http://localhost:3000"));

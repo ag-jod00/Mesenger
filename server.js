922aa70f0d51e22e000f25de5efa89dc40e48ba2
ag-jod00 <abhinandan02102010@gmail.com>
2025-02-22 10:00:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const io = socketIo(server);
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index01.html');
+    res.sendFile(__dirname + "/index.html");
 });
 
 io.on('connection', (socket) => {

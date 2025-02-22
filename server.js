8aa757f1107a0709a1552eb23ce24fb6dd2bbbd2
ag-jod00 <abhinandan02102010@gmail.com>
2025-02-22 08:34:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
@@ -10,10 +11,36 @@ const io = new Server(server);
 app.use(cors());
 app.use(express.static(__dirname));
 
-io.on("connection", (socket) => {
+// Connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/messengerDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("MongoDB Connection Error:", err));
+
+// Message Schema
+const MessageSchema = new mongoose.Schema({
+    name: String,
+    message: String,
+    timestamp: { type: Date, default: Date.now }
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+
+// When a client connects
+io.on("connection", async (socket) => {
     console.log("User connected:", socket.id);
 
-    socket.on("chatMessage", (data) => {
+    // Send previous messages from the database
+    const messages = await Message.find().sort({ timestamp: 1 });
+    socket.emit("previousMessages", messages);
+
+    // Listen for new messages
+    socket.on("chatMessage", async (data) => {
+        const newMessage = new Message(data);
+        await newMessage.save();
+
+        // Send message to all clients
         io.emit("chatMessage", data);
     });
 
@@ -22,6 +49,7 @@ io.on("connection", (socket) => {
     });
 });
 
+// Start server
 server.listen(3000, () => {
     console.log("Server running on port 3000");
 });
